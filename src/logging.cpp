4b8336e4b027cfc3deade83389938a4282a8d2df
Spar <developspartv@gmail.com>
2023-06-05 18:37:45 +0300

    separate by tab

--- src/logging.cpp
+++ src/logging.cpp
@@ -25,11 +25,11 @@ void Format(logging::record_view const& rec, logging::formatting_ostream& strm) 
 	namespace expr = boost::log::expressions;
 
 	auto ts = *rec[timestamp];
-	strm << '[' << rec[logging::trivial::severity] << "] [";
-	strm << to_iso_extended_string(ts) << "] ";
+	strm << '[' << rec[logging::trivial::severity] << "]\t[";
+	strm << to_iso_extended_string(ts) << "]\t";
 
 	if (!rec[file].empty()) {
-		strm << '[' << rec[file] << ':' << rec[file_line] << "] ";
+		strm << '[' << rec[file] << ':' << rec[file_line] << "]\t";
 	}
 
 	strm << rec[expr::smessage];
