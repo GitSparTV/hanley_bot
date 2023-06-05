97fa4912927bfcf8c79bc86da63fe73a07cf6076
Spar <developspartv@gmail.com>
2023-06-05 13:52:06 +0300

    Finish logging component

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,15 +1,61 @@
 #include "sdk.h"
 
+
+#include <boost/json.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/date_time.hpp>
 
-using namespace std::literals;
+#include "logging.h"
+
 namespace logging = boost::log;
 namespace sinks = boost::log::sinks;
-namespace keywords = boost::log::keywords;
-namespace expr = boost::log::expressions;
-namespace attrs = boost::log::attributes;
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
+
+namespace hanley_bot::logger {
+
+void Format(logging::record_view const& rec, logging::formatting_ostream& strm) {
+	namespace expr = boost::log::expressions;
+
+	//strm << boost::json::value{
+	//	{"timestamp", to_iso_extended_string(*rec[timestamp])},
+	//	{"data", *rec[additional_data]},
+	//	{"message", *rec[expr::smessage]}
+	//};
+
+	auto ts = *rec[timestamp];
+	strm << '[' << rec[logging::trivial::severity] << "] [";
+	strm << to_iso_extended_string(ts) << "] ";
+
+	if (rec[logging::trivial::severity] == logging::trivial::debug) {
+		strm << '[' << rec[file] << ':' << rec[file_line] << "] ";
+	}
+
+	strm << rec[expr::smessage];
+}
+
+void Init() {
+	namespace keywords = boost::log::keywords;
+
+	logging::add_common_attributes();
+
+	logging::add_file_log(
+		keywords::file_name = "logs/%Y-%m-%d.log",
+		keywords::format = &Format,
+		keywords::auto_flush = true,
+		keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0)
+	);
+
+	logging::add_console_log(
+		std::cout,
+		keywords::auto_flush = true,
+		keywords::format = &Format
+	);
+}
+
+} // namespace hanley_bot::logger
---
+++ src/logging.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "sdk.h"
+
+#include <filesystem>
+#include <string_view>
+
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(file, "File", std::string_view)
+BOOST_LOG_ATTRIBUTE_KEYWORD(file_line, "LineN", int)
+
+namespace hanley_bot::logger {
+
+void Init();
+
+constexpr std::string_view ExtractFileName(std::string_view file) {
+	return file.substr(file.find_last_of(std::filesystem::path::preferred_separator) + 1);
+}
+
+} // namespace hanley_bot::logger
+
+#define LOG_VERBOSE(severity) BOOST_LOG_TRIVIAL(severity) << boost::log::add_value("File",hanley_bot::logger::ExtractFileName(__FILE__)) << boost::log::add_value("LineN", __LINE__)
+#define LOG(severity) BOOST_LOG_TRIVIAL(severity)
