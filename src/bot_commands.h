3331557fb41ef81964cc19d684cbddf5077faabe
Spar <developspartv@gmail.com>
2023-06-05 13:54:02 +0300

    Remove cpp-only function

--- src/bot_commands.h
+++ src/bot_commands.h
@@ -11,7 +11,6 @@ namespace hanley_bot::commands {
 
 void PushCommands(hanley_bot::Bot& bot);
 void InitializeCommands(hanley_bot::Bot& bot);
-void ParseNumber(std::string_view& course_id, int& course_id_number, bool& retFlag);
 void CallCommand(hanley_bot::Bot& bot, std::string path, const domain::Context& context);
 
 } //namespace hanley_bot::commands
