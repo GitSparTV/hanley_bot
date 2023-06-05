bd93af71a21265275dea93df82597cfba314ca74
Spar <developspartv@gmail.com>
2023-06-05 13:53:43 +0300

    Finally commit main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,43 @@
-#include <stdio.h>
-#include <tgbot/tgbot.h>
+#include <iostream>
 
-int main() {
-    TgBot::Bot bot("PLACE YOUR TOKEN HERE");
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
-    });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
-            return;
-        }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
-    });
-    try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        TgBot::TgLongPoll longPoll(bot);
-        while (true) {
-            printf("Long poll started\n");
-            longPoll.start();
-        }
-    } catch (TgBot::TgException& e) {
-        printf("error: %s\n", e.what());
-    }
-    return 0;
+#include "logging.h"
+#include "bot.h"
+#include "bot_commands.h"
+#include "config_json_reader.h"
+
+using namespace std::chrono_literals;
+
+int main(int argc, char* argv[]) {
+	std::vector<std::string_view> args(argv + 1, argv + argc);
+
+	if (argc < 2) {
+		std::cout << "./hanley_bot <config_file> [<push_to_telegram>]" << std::endl;
+
+		return EXIT_FAILURE;
+	}
+
+	hanley_bot::logger::Init();
+
+	auto config = hanley_bot::config::FromJSONFile(args[0]);
+
+	try {
+		hanley_bot::Bot bot(config);
+
+		if (args.size() >= 2 && args[1] == "push") {
+			LOG(info) << "Pushing commands to Telegram";
+
+			hanley_bot::commands::PushCommands(bot);
+		}
+
+		hanley_bot::commands::InitializeCommands(bot);
+
+		bot.Run();
+	} catch (const std::exception& ex) {
+		std::cout << "Exception caught in main (" << typeid(ex).name() << "): " << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	} catch (...) {
+		std::cout << "Unknown exception caught in main" << std::endl;
+	}
+
+	return EXIT_SUCCESS;
 }
